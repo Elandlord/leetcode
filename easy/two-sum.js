8565be7391823dcb89a8769bbf50683dddb11795
Eric Landheer <e.landheer@mentechmedia.nl>
2021-09-06 17:25:35 +0200

    :rainbow: Add 2nd solution for twoSum

--- easy/two-sum.js
+++ easy/two-sum.js
@@ -12,3 +12,21 @@ var twoSum = function(nums, target) {
         }
     }
 };
+
+// Solution 2 (using maps)
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let map = new Map();
+
+    for(let i = 0; i < nums.length; i++) {
+        let remainder = target - nums[i];
+
+        if(map.has(remainder)) return [map.get(remainder), i];
+
+        map.set(nums[i], i)
+    }
+};
