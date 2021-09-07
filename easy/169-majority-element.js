9febb6ccdceac048ada84a6fda52c107213ea80c
Eric Landheer <e.landheer@mentechmedia.nl>
2021-09-07 10:11:17 +0200

    :rainbow: Add solution 2 (sorting) and solution 3 (Boyer-Moore voting algorithm)

--- easy/169-majority-element.js
+++ easy/169-majority-element.js
@@ -1,3 +1,5 @@
+// Solution 1 with Object (map)
+
 /**
  * @param {number[]} nums
  * @return {number}
@@ -16,3 +18,38 @@ var majorityElement = function(nums) {
 };
 
 console.log(majorityElement([2,2,1,1,1,2,2])); // 2
+
+// Solution 2 (Sorting and taking n/2 value
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    nums.sort();
+    return nums[Math.floor(nums.length / 2)];
+};
+
+// Solution 3 (Boyer-Moore Voting Algorithm)
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let count = 0;
+    let candidate = null;
+
+    for (num of nums) {
+        if (count === 0) {
+            candidate = num;
+        }
+
+        count += (candidate === num) ? 1 : -1;
+    }
+
+    return candidate;
+};
+
+console.log(majorityElement([3,2,3])); // 3
+console.log(majorityElement([2,2,1,1,1,2,2])); // 2
+
+
