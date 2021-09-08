958cac01a3f310a914700feb5bb364841212fcb5
Eric Landheer <e.landheer@mentechmedia.nl>
2021-09-08 13:26:51 +0200

    :rainbow: 637 - accepted results

--- medium/637-shifting-letters.js
+++ medium/637-shifting-letters.js
@@ -4,29 +4,16 @@
  * @return {string}
  */
 var shiftingLetters = function(s, shifts) {
-    String.prototype.replaceAt = function(index, replacement) {
-        if (index >= this.length) return this.valueOf();
-        return this.substring(0, index) + replacement + this.substring(index + 1);
-    }
+    let total = 0;
+    let splittedString = s.split('').reverse();
 
-    function shiftLetter(str, times) {
-        for(let i = 0; i < str.length; i++) {
-            let number = str.charCodeAt(i) - 96;
-
-            number = number+times;
-            if(number >= 26) number = number%26 === 0 ? 26 : number%26;
-
-            str = str.replaceAt(i, String.fromCharCode(96 + number));
-        }
-        return str;
-    }
-
-    for(let i = 0; i < shifts.length; i++) {
-        let substr1 = shiftLetter(s.substring(0, i+1), shifts[i]);
-        let substr2 = s.substring(i+1);
-        s = substr1 + substr2;
-    }
-    return s;
+    return shifts.reverse().map((shift, index) => {
+        total += shift;
+        let number = (splittedString[index].charCodeAt(0) - 96) + total;
+        number = number%26 === 0 ? 26 : number%26;
+        splittedString[index] = String.fromCharCode(96 + number);
+        return splittedString[index];
+    }).reverse().join('');
 };
 
 console.log(shiftingLetters("abc", [3,5,9]));
