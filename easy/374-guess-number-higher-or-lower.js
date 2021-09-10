0b269a6f84a0c35b617b92d34906e081697a8b35
Eric Landheer <e.landheer@mentechmedia.nl>
2021-09-10 08:36:15 +0200

    :rainbow: SOlve 374 - guess number higher or lower using binary search

---
+++ easy/374-guess-number-higher-or-lower.js
@@ -0,0 +1,30 @@
+/**
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	            -1 if num is lower than the guess number
+ *			             1 if num is higher than the guess number
+ *                       otherwise return 0
+ * var guess = function(num) {}
+ */
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var guessNumber = function(n) {
+
+    let start = 0;
+
+    while(start <= n) {
+        let mid = Math.floor((start + n) / 2);
+
+        let result = guess(mid);
+
+        if(result === 0) return mid;
+
+        if(result === 1) {
+            start = mid+1;
+        } else {
+            n = mid-1;
+        }
+    }
+};
