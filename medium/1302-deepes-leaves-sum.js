8960ed851a6e14b09d65591f43f5812ff82620bc
Eric Landheer <e.landheer@mentechmedia.nl>
2021-09-12 12:31:18 +0200

    :rainbow: Solution to 1302 - deepest leaves sum

--- medium/1302-deepes-leaves-sum.js
+++ medium/1302-deepes-leaves-sum.js
@@ -1,35 +1,22 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root
- * @return {number}
- */
 var deepestLeavesSum = function(root) {
-    let levelMap = {};
+    let levelMap = {}; // create map to store all the nodes of each level in.
 
-    function traverse(node, level = 0) {
+    function traverse(node, level = 0) { // declare function to traverse the tree (note the `level` parameter, which defaults to zero).
 
-        if(node === null) return;
+        if(node === null) return; // return null if the node does not exist
 
-        if(levelMap[level] === undefined) levelMap[level] = [];
+        if(levelMap[level] === undefined) levelMap[level] = []; // initialise an array for this specific level (see `level` parameter).
 
-        levelMap[level].push(node.val);
+        levelMap[level].push(node.val); // push the value of the current node to the array
 
-        level++;
+        level++; // increment the level
 
-        traverse(node.left, level);
-        traverse(node.right, level);
+        traverse(node.left, level); // visit the left child node, passing the incremented level
+        traverse(node.right, level); // visit the right child node, passing the incremented level
     }
 
-    traverse(root);
-
+    traverse(root); // call the traverse function with the root
 
     return Object.values(levelMap)[Object.values(levelMap).length - 1]
-        .reduce((a, b) => a+b, 0);
+        .reduce((a, b) => a+b, 0); // return the last index of the object, and use reduce to sum all the values of the nodes in that level.
 };
