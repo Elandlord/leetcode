8f7a6eee19cb9cee0a8307e2581d167d05e32cc7
Eric Landheer <e.landheer@mentechmedia.nl>
2021-10-23 11:48:48 +0200

    Create 1221-split-a-string-in-balanced-strings.js

---
+++ easy/1221-split-a-string-in-balanced-strings.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var balancedStringSplit = function(s) {
+    let count = 0;
+    let balance = 0;
+    
+	for (let i = 0; i < s.length; i ++) {
+        s[i] === 'R' ? balance++ : balance--;
+        if(balance === 0) count++;
+	}
+    
+	return count;
+};
